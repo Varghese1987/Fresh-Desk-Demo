f9db58403e39789d4860f51462271c2986c8c67d
varghese <varghese87es@gmail.com>
2020-05-23 04:08:45 +0530

    commit

--- freshdesk.js
+++ freshdesk.js
@@ -1,4 +1,5 @@
 document.body.innerHTML = `
+<div class = "container cont2">
 <div class="accordion" id="accordionExample">
   <div class="card">
     <div class="card-header" id="headingOne">
@@ -13,7 +14,7 @@ document.body.innerHTML = `
       <div class="card-body">
 
         <p>
-          <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Create Ticket</a>
+        <center><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Create a New Ticket</a></center>
         </p>
         <div class="collapse" id="collapseExample">
           <div class="card card-body">
@@ -48,7 +49,7 @@ document.body.innerHTML = `
       <div class="card-body">
         
         <p>
-          <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Create Contact</a>
+          <center><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Create a new Contact</a></center>
         </p>
         <div class="collapse" id="collapseExample">
           <div class="card card-body">
@@ -80,6 +81,7 @@ document.body.innerHTML = `
   </div>
   
 </div>
+</div>
 `
 
 $(document).ready(function () {
@@ -95,7 +97,7 @@ $(document).ready(function () {
       headers: { "Authorization": "Basic " + btoa(api_key + ":x") },
       success: function (data) {
         console.log(data);
-
+        console.log(data[0].id)
         function template(ticket) {
           return `
         <div class="card col-lg-12">
@@ -103,13 +105,17 @@ $(document).ready(function () {
           <div class="card-body">
             <h5 class="card-title">Special title treatment</h5>
             <p class="card-text">${ticket.description}</p>
-            <button type="button" class="btn btn-primary">Edit</button>
+            
           </div>
-          <div class="card-footer bg-transparent border-success">Footer</div>
+          <div class="card-footer bg-transparent border-success">
+          <button id = "but${ticket.id}" type="button" onclick = "deleteTicket('${ticket.id}')" class="btn btn-primary">Delete</button>
+          </div>
         </div>
         `
+
         }
         document.getElementById('ticket').innerHTML = `${data.map(template).join('')}`
+        
       },
       error: function () {
         alert("Page Load Error");
@@ -154,17 +160,19 @@ $(document).ready(function () {
 
         function template(item) {
           return `
-        
+        <div class = "col-lg-3">
         <div class="card">
           <h5 class="card-header">${item.name}</h5>
           <div class="card-body">
             <p class="card-text">${item.email}</p>
             <p class="card-text">${item.phone}</p>
-            <button type="button" class="btn btn-primary">Edit</button>
+            
           </div>
-          <div class="card-footer bg-transparent border-success">Footer</div>
+          <div class="card-footer bg-transparent border-success">
+          <button id = "but${item.id}" type="button" onclick = "deleteContact('${item.id}')" type="button" class="btn btn-primary">Delete</button>
+          </div>
         </div>
-        
+        </div>
         `
         }
         document.getElementById('contact').innerHTML = `${data.map(template).join('')}`
@@ -199,6 +207,54 @@ $(document).ready(function () {
       );
       //location.reload(true);
     }
-  );
+  );      
 
 });
+
+function deleteTicket(value){
+
+  var yourdomain = 'varghese87es';
+  var api_key = '2lvC7tLptQmim0c4QmEn';
+  var ticket_id = value;
+
+  $.ajax(
+    {
+      url: "https://" + yourdomain + ".freshdesk.com/api/v2/tickets/"+ticket_id,
+      type: 'DELETE',
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      headers: { "Authorization": "Basic " + btoa(api_key + ":x") },
+      success: function () {
+        alert("Data Deleted");
+        location.reload(true);
+      },
+      error: function () {
+        alert("Page Load Error");
+      }
+    });
+  
+}
+
+function deleteContact(value){
+
+  var yourdomain = 'varghese87es';
+  var api_key = '2lvC7tLptQmim0c4QmEn';
+  var ticket_id = value;
+
+  $.ajax(
+    {
+      url: "https://" + yourdomain + ".freshdesk.com/api/v2/contacts/"+ticket_id,
+      type: 'DELETE',
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      headers: { "Authorization": "Basic " + btoa(api_key + ":x") },
+      success: function () {
+        alert("Data Deleted");
+        location.reload(true);
+      },
+      error: function () {
+        alert("Page Load Error");
+      }
+    });
+  
+}
